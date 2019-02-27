5feca9270fe294205e02f06ca00932832be982fc
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-27 14:45:51 -0700

    db setup

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const server = express();
+
+db = require('../data/dbConfig.js');
 
 server.use(helmet());
 server.use(express.json());
